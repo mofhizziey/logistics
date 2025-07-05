fa1ba538f54ba4f4e604af40cda372d42be6547e
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-07-05 09:28:07 -0400

    nionin

--- logistics/settings.py
+++ logistics/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 'yes', 'on')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 
