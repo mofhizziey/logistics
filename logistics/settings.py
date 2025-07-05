a421326742b89c89a73cc18d233f158204f2190b
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-07-05 08:03:08 -0400

    dbwbud

--- logistics/settings.py
+++ logistics/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
